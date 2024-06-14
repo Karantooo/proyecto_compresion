d62075f47a25897e5fa215420856f0dfcaa5322e
Ricardo Charris <rica14062004@gmail.com>
2024-06-14 17:28:28 -0400

    Posible version final del trie

--- Lempel_Ziv.h
+++ Lempel_Ziv.h
@@ -2,11 +2,11 @@
 #define LEMPEL_ZIV_H
 
 #include <iostream>
+#include "trie.h"
 
-//Lempel_ziv debe devolver un archivo binario con la compresion del mensaje y el trie para descomprimir el archivo
-//Implementar trie
-std::pair<trie, std::string> Lempel_Ziv(std::string mensaje){
-    trie compression_trie = new trie();
+//Lempel_ziv debe devolver un archivo con la compresion del mensaje y el trie para descomprimir el archivo
+std::pair<TrieNode, std::string> Lempel_Ziv(std::string mensaje){
+    TrieNode* compression_trie = new TrieNode();
     std::string codificacion;
     std::string palabra;
     std::string tmp_codificacion;
@@ -16,7 +16,7 @@ std::pair<trie, std::string> Lempel_Ziv(std::string mensaje){
         if(mensaje[i] == ' ' && !palabra.empty()){
             std::pair<int, int>* rango_palabra;
             //El metodo find devuelve un pair con un indice al inicio y al final de la palabra, o nullptr si no se encontro antes en el mensaje
-            rango_palabra = compression_trie.find(palabra);
+            rango_palabra = search_key(compression_trie, palabra);
             //Si la palabra estaba en el trie, se añade a la codificacion un parentesis con el indice inicial y final
             if(rango_palabra!=nullptr){
                 codificacion.append('('+std::to_string(rango_palabra->first)+','+std::to_string(rango_palabra->second));
@@ -35,14 +35,14 @@ std::pair<trie, std::string> Lempel_Ziv(std::string mensaje){
 
 
         std::pair<int, int>* rango_caracter;
-        rango_caracter = compression_trie.find(mensaje[i]);
+        rango_caracter = search_key(compression_trie, std::to_string(mensaje[i]));
     
         //Verifica si el caracter aparecio anteriormente y crea una codificacion temporal caracter a caracter
         if(rango_caracter == nullptr){
             compression_trie.insert(mensaje[i], i);
-            tmp_codificacion.append("("+mensaje[i]+')');
+            tmp_codificacion.append("("+mensaje[i]+','+std::to_string(0)+')');
         } else{
-            tmp_codificacion.append("("+mensaje[i]+','+std::to_string(rango_caracter->first)+')');
+            tmp_codificacion.append("("+std::to_string(rango_caracter->first)+','+std::to_string(rango_caracter->second)+')');
         }
     }
 }
--- trie.h
+++ trie.h
@@ -1,39 +1,62 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 struct TrieNode {
 
     // pointer array for child nodes of each node
-    TrieNode* childNode[26];
+    TrieNode* childNode[27];
 
     // Used for indicating ending of string
     bool wordEnd;
+    int begin;
+    int end;
 
-    TrieNode()
-    {
+    TrieNode(){
         // constructor
         // initialize the wordEnd variable with false
         // initialize every index of childNode array with
         // NULL
         wordEnd = false;
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < 27; i++) {
+            childNode[i] = NULL;
+        }
+    }
+    TrieNode(int begin, int end) : begin(begin), end(end)
+    {
+        // constructor
+        // initialize the wordEnd variable with false
+        // set the begin and the end index in the message of the word that finishes with the current character representated by this node.
+        // initialize every index of childNode array with
+        // NULL
+        wordEnd = false;
+        for (int i = 0; i < 27; i++) {
             childNode[i] = NULL;
         }
     }
 };
 
-void insert_key(TrieNode* root, string& key)
+void insert_key(TrieNode* root, std::string key, int begin)
 {
-    // Initialize the currentNode pointer
-    // with the root node
+    // Initialize the currentNode pointer with the root node and the end index of the word.
     TrieNode* currentNode = root;
+    int end = begin - 1;
 
     // Iterate across the length of the string
     for (auto c : key) {
-
+        end++;
         // Check if the node exist for the current
         // character in the Trie.
-        if (currentNode->childNode[c - 'a'] == NULL) {
+
+        
+        if(c == ' ' && currentNode->childNode[26] == NULL){
+            // If node for current character does not exist
+            // then make a new node
+            TrieNode* newNode = new TrieNode();
+
+            // Keep the reference for the newly created
+            // node.
+            currentNode->childNode[26] = newNode;
+        }
+        else if (c != ' ' && currentNode->childNode[tolower(c) - 'a'] == NULL) {
 
             // If node for current character does not exist
             // then make a new node
@@ -41,84 +64,67 @@ void insert_key(TrieNode* root, string& key)
 
             // Keep the reference for the newly created
             // node.
-            currentNode->childNode[c - 'a'] = newNode;
+            currentNode->childNode[tolower(c) - 'a'] = newNode;
+           
+            
         }
 
         // Now, move the current node pointer to the newly
         // created node.
-        currentNode = currentNode->childNode[c - 'a'];
+        if(c != ' '){
+                currentNode = currentNode->childNode[tolower(c) - 'a'];
+            } else{
+                currentNode = currentNode->childNode[26];
+            }
     }
 
     // Increment the wordEndCount for the last currentNode
     // pointer this implies that there is a string ending at
-    // currentNode.
-    currentNode->wordEnd = 1;
+    // currentNode and set the begin and end index of the word.
+
+    currentNode->wordEnd = true;
+    currentNode->begin = begin;
+    currentNode->end = end;
 }
 
-bool search_key(TrieNode* root, string& key)
+std::pair<int, int>* search_key(TrieNode* root, std::string key)
 {
-    // Initialize the currentNode pointer
-    // with the root node
+    // Initialize the currentNode pointer with the root node, the pair that will be returned and a bool value to verify if the word is only a character
     TrieNode* currentNode = root;
+    std::pair<int, int>* word_index_range = new std::pair<int, int>();
+    bool is_char = key.size() == 1;
 
     // Iterate across the length of the string
     for (auto c : key) {
 
         // Check if the node exist for the current
         // character in the Trie.
-        if (currentNode->childNode[c - 'a'] == NULL) {
+        if(c == ' ' && currentNode->childNode[26] == NULL){
+            // Given word does not exist in Trie
+            return nullptr;
+        }
+
+        if (c != ' ' && currentNode->childNode[tolower(c) - 'a'] == NULL) {
 
             // Given word does not exist in Trie
-            return false;
+            return nullptr;
         }
 
         // Move the currentNode pointer to the already
         // existing node for current character.
-        currentNode = currentNode->childNode[c - 'a'];
+        if(c != ' '){
+                currentNode = currentNode->childNode[tolower(c) - 'a'];
+            } else{
+                currentNode = currentNode->childNode[26];
+            }
     }
 
-    return (currentNode->wordEnd == true);
-}
-
-// Driver code
-int main()
-{
-    // Make a root node for the Trie
-    TrieNode* root = new TrieNode();
-
-    // Stores the strings that we want to insert in the
-    // Trie
-    vector<string> inputStrings
-        = { "and", "ant", "do", "geek", "dad", "ball" };
-
-    // number of insert operations in the Trie
-    int n = inputStrings.size();
-
-    for (int i = 0; i < n; i++) {
-        insert_key(root, inputStrings[i]);
+    // If the final character of key is the final of a word, returns begin and end index, if not return a nullptr
+    if(currentNode->wordEnd == true){
+        word_index_range->first = currentNode->begin;
+        word_index_range->second = currentNode->end;
+        return word_index_range;
+    }else{
+        return nullptr;
     }
-
-    // Stores the strings that we want to search in the Trie
-    vector<string> searchQueryStrings
-        = { "do", "geek", "bat" };
-
-    // number of search operations in the Trie
-    int searchQueries = searchQueryStrings.size();
-
-    for (int i = 0; i < searchQueries; i++) {
-        cout << "Query String: " << searchQueryStrings[i]
-             << "\n";
-        if (search_key(root, searchQueryStrings[i])) {
-            // the queryString is present in the Trie
-            cout << "The query string is present in the "
-                    "Trie\n";
-        }
-        else {
-            // the queryString is not present in the Trie
-            cout << "The query string is not present in "
-                    "the Trie\n";
-        }
-    }
-
-    return 0;
 }
