6c84bd91f5cbd0b810188612b755bf689b308460
martinfigue <martin.figueroahdez@gmail.com>
2024-06-17 13:20:29 -0400

    feat: comentarios retirados

--- Lempel_Ziv_Descompresion.h
+++ Lempel_Ziv_Descompresion.h
@@ -36,41 +36,5 @@ void decode(std::string ruta_archivo){
     decodificado << mensaje;
     file.close();
     decodificado.close();
-
-    /*for(int i = 0; i < codificacion.length(); i++){
-        c = codificacion[i];
-        if(c == '('){
-            for(int j = i + 1; codificacion[j] != ','; j++){
-                if(!isdigit(codificacion[j])){
-                    mensaje += codificacion[j];
-                    break;
-                }
-                else if(isdigit(codificacion[j])){
-                    posicion += codificacion[j];
-                }
-            }
-        }
-        else if(c == ','){
-            for(int j = i + 1; codificacion[j] != ')'; j++){
-                if(codificacion[j] == '0'){
-                    break;
-                }
-                else if(isdigit(codificacion[j])){
-                    cantidad += codificacion[j];
-                }
-            }
-        }
-        else if(c == ')'){
-            if(codificacion[i - 1] != '0'){
-                for(int j = stoi(posicion); j < stoi(cantidad); j++){
-                    mensaje += mensaje[j];
-                }
-                posicion.clear();
-                cantidad.clear();
-            }
-        }
-    }
-    return mensaje;
-    */
 }
 #endif
