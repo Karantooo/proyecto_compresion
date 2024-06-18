72e22c64930450c414f1dc3851bb6d7ded388aff
martinfigue <martin.figueroahdez@gmail.com>
2024-06-17 21:19:13 -0400

    feat: variable booleana removida de la descompresion

--- Lempel_Ziv_Descompresion.h
+++ Lempel_Ziv_Descompresion.h
@@ -16,22 +16,15 @@ void decode(std::string ruta_archivo){
     file.open(ruta_archivo, std::ios::in | std::ios::binary);
     std::ofstream decodificado("decode.txt");
     while(file.peek() != EOF){
-        bool int_flag;
         int index;
         int module;
-        file.read(reinterpret_cast<char*> (&int_flag), sizeof(bool));
         file.read(reinterpret_cast<char*> (&index), sizeof(int));
         file.read(reinterpret_cast<char*> (&module), sizeof(int));
-        if(module == 0){
-            if(!int_flag){
-                unsigned char c = static_cast<unsigned char>(index);
-                std::string tmp_string(1, c);
-                mensaje.append(tmp_string);
-            }else{
-                mensaje.append(std::to_string(index));
-            }
+        if(!module){
+            unsigned char c = static_cast<unsigned char>(index);
+            std::string tmp_string(1, c);
+            mensaje.append(tmp_string);
         }
-        
         else{
             for(int i = index; i < index + module; i++){
                 mensaje += mensaje[i];
