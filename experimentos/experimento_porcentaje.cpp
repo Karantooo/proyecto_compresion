68500b9609ef76300efb33860e740b496f3faf4c
martinfigue <martin.figueroahdez@gmail.com>
2024-06-20 20:38:19 -0400

    add: experimento porcentaje compresion

---
+++ experimentos/experimento_porcentaje.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, char* argv[]){
+	std::filesystem::path filename1=argv[1];
+	std::filesystem::path filename2=argv[2];
+	
+	double filesize1= std::filesystem::file_size(filename1.c_str());
+	double filesize2= std::filesystem::file_size(filename2.c_str());
+	
+	std::cout<<filesize1<<" "<<filesize2<<std::endl;
+	double quotient;
+	quotient=filesize1/filesize2;
+	quotient=quotient*100;
+	std::cout<<"Porcentaje de compresion "<<quotient<<std::endl; 
+}
