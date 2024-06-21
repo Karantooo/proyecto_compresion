dabfed792dceb4aef9256e7aecf95b6c94244e58
martinfigue <martin.figueroahdez@gmail.com>
2024-06-21 16:44:10 -0400

    style: para la memoria, huffmann y lz van en archivos distintos

---
+++ experimento_compresion_LZ_memoria.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../Lempel_Ziv.h"
+#include "../huffman.h"
+
+int main(int argc, char* argv[]){
+	Lempel_ziv compresor(argv[1]);
+    compresor.compress();
+}
--- experimentos/experimento_codificacion_huffman_memoria.cpp
+++ experimentos/experimento_codificacion_huffman_memoria.cpp
@@ -5,6 +5,4 @@
 int main(int argc, char* argv[]){
 	HuffmanCoding codificador(argv[1]);
     codificador.enconde();
-	Lempel_ziv compresor(argv[1], std::stoi(argv[2]));
-    compresor.compress();
 }
