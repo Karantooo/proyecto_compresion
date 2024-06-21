d187cc25ef40b632465e94c88421ce8e19dd02fe
martinfigue <martin.figueroahdez@gmail.com>
2024-06-21 16:37:39 -0400

    feat: se calcula el tamanio de los archivos en descompresion

--- experimentos/experimento_decompresion.cpp
+++ experimentos/experimento_decompresion.cpp
@@ -2,8 +2,16 @@
 #include <chrono>
 #include "../Lempel_Ziv.h"
 #include "../huffman.h"
+#include <filesystem>
 
 int main(int argc, char* argv[]){
+    std::filesystem::path filename1 = argv[1];
+    std::filesystem::path filename2 = argv[2];
+    double filesize1 = std::filesystem::file_size(filename1.c_str());
+    filesize1 /= 1024;
+	double filesize2 = std::filesystem::file_size(filename2.c_str());
+    filesize2 /= 1024;
+
     HuffmanCoding decodificador(argv[1]);
     auto start = std::chrono::high_resolution_clock::now();
     decodificador.decode();
@@ -17,5 +25,5 @@ int main(int argc, char* argv[]){
     end = std::chrono::high_resolution_clock::now();
     double decompressing_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     decompressing_time *= 1e-9;
-    std::cout << decoding_time << ";" << decompressing_time << std::endl;
+    std::cout << filesize1 << ";" << decoding_time << ";" << filesize2 << ";" << decompressing_time << std::endl;
 }
