ccb5c9a39156b3b381f14753ab29199ab77caab8
martinfigue <martin.figueroahdez@gmail.com>
2024-06-21 12:24:20 -0400

    add: script para las compresiones

---
+++ experimentos/run_experimento_compresion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+files = ("cancion.txt", "mio_cid.txt", "shakespeare.txt", "smash_wikipedia.txt", "texto.txt")
+
+for ((c = 1; c <= 20; c++)
+do
+	for n in "${files[@]}"
+	do
+		./experimento_compresion.cpp "$n" 20
+	done
+done
