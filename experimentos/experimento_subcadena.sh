75230eed812b8b4b3e2444b534ccf06b733fe54a
martinfigue <martin.figueroahdez@gmail.com>
2024-06-22 16:30:22 -0400

    feat: experimento subcadena listo

--- experimentos/experimento_subcadena.sh
+++ experimentos/experimento_subcadena.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for( c = 1; c <= 20; c++)
+output_file="resultados_subcadena.csv"
+
+echo "size subcadena;tiempo compresion LZ;tiempo descompresion LZ"
+
+for(( c = 1; c <= 20; c++))
 do
-	for n in 5 10 15 20 25 30
+	for n in 5 10 15 20
 	do
-		./experimento_compresion_LZ.cpp "shakespeare.txt" $n
+		./experimento_compresion_LZ "../textos/cancion.txt" $n >> $output_file
 	done
 done
