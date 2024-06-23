22d322030b780bfe1d466bbb7763bd91577da4ae
martinfigue <martin.figueroahdez@gmail.com>
2024-06-23 02:20:25 -0400

    fix: error de sintaxis en .sh de experimento huffman

--- experimentos/run_experimento_tiempo_huffman.sh
+++ experimentos/run_experimento_tiempo_huffman.sh
@@ -2,7 +2,7 @@
 
 
 echo "size archivo;tiempo codificacion Huffman"
-files=("../textos/texto_1mb.txt ../textos/texto_2mb.txt ../textos/texto_3mb.txt ../textos/texto_5mb.txt ../textos/texto_10mb.txt")
+files=("../textos/texto_1mb.txt" "../textos/texto_2mb.txt" "../textos/texto_3mb.txt" "../textos/texto_5mb.txt" "../textos/texto_10mb.txt")
 
 for ((c=1; c<=20; c++))
 do
