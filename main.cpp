8a3674c00ab2c4519a9c065e21d8c4cebdb4faa3
martinfigue <martin.figueroahdez@gmail.com>
2024-06-23 01:43:41 -0400

    add: archivos de texto

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Lempel_Ziv.h"
-
-int main(){
-
-    // std::string texto = "./LZ_compression.dat";
-    std::string texto = "../textos/shakespeare.txt";
-    Lempel_ziv compresor(texto, 10);
-    compresor.compress();
-}
