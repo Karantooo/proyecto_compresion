6eaabad47d24536a8f7219d9d1e9185aab6a074c
karantooo <ctomasalvarezn@gmail.com>
2024-06-23 20:07:32 -0400

    feat: se recopilan mas datos para experimento compresion

--- experimentos/run_experimento_compresion.sh
+++ experimentos/run_experimento_compresion.sh
@@ -3,7 +3,7 @@
 output_file="resultados_compresion_desscompresion.csv"
 
 echo "size archivo;tiempo codificacion Huffman;tiempo compresion LZ;tiempo decodificacion Huffmann;tiempo decompresion LZ"
-files=("../textos/cancion.txt" "../textos/mio_cid.txt" "../textos/shakespeare.txt" "../textos/smash_wikipedia.txt" "../textos/texto.txt")
+files=("../textos/texto_1mb.txt" "../textos/texto_750kb.txt" "../textos/texto_500kb.txt")
 
 for ((c=1; c<=20; c++))
 do
